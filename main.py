fe8b3587f906c3f4392a1ab9b3dd630eaf06ea54
DmitPSh <dshevchuk131313@gmail.com>
2024-10-07 23:29:09 +0300

    third var_7.10.24

--- main.py
+++ main.py
@@ -5,7 +5,9 @@
 #     print( i, '=',      list_[i])
 
 
-list=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+list=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+prime = []
+not_prime = []
 print(len(list))
 print(list[len(list)-1])
 
@@ -14,14 +16,17 @@ for i in range(2,len(list)):
      for j in range(1, len(list)):
           if list[i] % list[j] != 0:
                print('number', list[i],  '= prime')
+               prime.append(list[i])
                break
 
 
           else:
                print('number', list[i],  '= not_prime')
+               not_prime.append(list[i])
                break
 
-
+print(not_prime)
+print(prime)
 
 
 
