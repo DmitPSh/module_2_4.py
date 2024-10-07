2e682ef68c24d162248d80f99f24c76b20fa0a43
DmitPSh <dshevchuk131313@gmail.com>
2024-10-08 00:02:46 +0300

    first_var_8.10.24

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
-#
-# list_=[12,34,88,21,92,22]
-# print(len(list_))
-# for i in range(0,len(list_)):
-#     print( i, '=',      list_[i])
 
 
 list=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 prime = []
 not_prime = []
-print(len(list))
-print(list[len(list)-1])
+
 
 for i in range(2,len(list)):
      print('list [',i,']=',list[i])
@@ -27,52 +21,4 @@ for i in range(2,len(list)):
 
 print(not_prime)
 print(prime)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-         # for i in range(0, 20):
-#     print('arr ', '[', i, '] ==' , i + 5 )
-#
-# for i in range(1, 10):
-#     print(' ')
-#     for j in range(1, 10):
-#         print( i,' ** ', j, ' = ', i**j  )
-#
-#         # print(f"{i} x {j} = {i * j}")
-
-
-#
-# #numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-# primes = []
-# not_primes = []
-# numbers = [1, 2, 3, 4, 5, 6]
-# print(len(numbers))
-#
-#
-#
-#
-#
-#
-#
-# # if numbers[4] % numbers[3] != 0:
-# #     print('Simple')
-
-
-
-
-
-
-
-
 
